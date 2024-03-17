b4ede9046bac27638cd291789bd3c3d0411357b1
Mr.chicken <chicken.science.lab@outlook.com>
2024-03-17 14:29:57 -0600

    Start finishing the cleaned up version

--- redo.js
+++ redo.js
@@ -21,6 +21,10 @@ function pathtopath(name,location){
       if(!/\.json$/.test(location)){location += ".json";}
       return location
 }
+function sha256(content) {  
+  return crypyo.createHash('sha256').update(content).digest('binary')
+}
+var self ;
 module.exports = class main{
     constructor(){
         console.log("test")
@@ -40,19 +44,18 @@ location = pathtopath(name,location)
             location;
             constructor(name,location){
                 this.location = pathtopath(name,location)
-
+              self = this;
                 console.log(this.location);
               }
-              #sha256(content) {  
-                return crypyo.createHash('sha256').update(content).digest('binary')
-              }
+
               #databasedata
               #refreshdata(){
                 this.#databasedata = JSON.parse(fs.readFileSync(this.location,'utf8'));
               }
+
               addacount(username, pw) {
                 this.#refreshdata();
-                var password = this.#sha256(pw);
+                var password = sha256(pw);
                 if (this.#databasedata[username] != undefined) {
                   //console.error("username taken");
                   throw new Error("username taken");
@@ -67,29 +70,45 @@ location = pathtopath(name,location)
                 fs.writeFileSync(this.location, JSON.stringify(temp));
                 return [username, password];
               }
-              login(username, password) {
-                this.#refreshdata();
-                var userdata = this.#databasedata[username];
-                if (userdata == undefined) {
-                  console.log("no acount");
-                  return false;
-                } else if (userdata.password != this.#sha256(password)) {
-                  throw new Error("incorect passowrd");
-                } else if (userdata.password === this.#sha256(password)) {
-                  try {
-                    var getdc = function () {
-                      console.log(aes256.decrypt(password, userdata["data"]));
-                      return JSON.parse(aes256.decrypt(password, userdata["data"]));
-                    };
-                    var dc = getdc();
-                  } catch (err) {
-                    console.error(err);
-                    return false;
+              login(...args){
+                return new class user{
+                  #unencripted
+                  #username
+                  #password
+                  #userdata
+                  #refreshud(){
+                    self.#refreshdata();
+                    this.#userdata = self.#databasedata[this.#username];
                   }
-                }
-              }          
+                  #decript(){
+                    self.#refreshdata();
+                    this.#unencripted = JSON.parse(aes256.decrypt(this.#password, userdata["data"]));
+                  }
+                  constructor(username, password){
+                    this.#refreshud()
+                    this.#username = username;
+                    this.#password = password;
+                    if (this.#userdata == undefined) {
+                      console.log("no acount");
+                      return false;
+                    } else if (this.#userdata.password != sha256(password)) {
+                      throw new Error("incorect passowrd");
+                    } else if (this.#userdata.password === sha256(password)) {
+                      try {
+                        this.#decript();
+                        console.log(this.#unencripted);
+                      } catch (err) {
+                        console.error(err);
+                        return false;
+                      }
+                    }
+                  }
+                }(...args)
+              }
+          
           }(...args)
       }
 }
-// cd node_modules/easynodedatabase
 //var database = require("./redo.js")
+//database.get("tests/databases/foodata.json").login("chicen", "bocker99")
+//database.get("tests/databases/foodata.json").addacount("chicen", "bocker99");
